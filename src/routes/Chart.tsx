5915960b47e9bc1b47110ff4e2c946ce9e59a93a
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-15 17:18:26 +0900

    feat: Create coin price chart

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,90 @@
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface ChartProps {
+  coinId: string;
+}
+
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId),
+    {
+      refetchInterval: 10000,
+    }
+  );
+  return (
+    <h1>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => Number(price.close)) as number[],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 500,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 5,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              labels: {
+                show: false,
+                datetimeFormatter: {
+                  month: "MMM 'yy",
+                },
+              },
+              axisTicks: { show: false },
+              type: "datetime",
+              categories: data?.map((price) => price.time_close * 1000),
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$ ${value}`,
+              },
+            },
+          }}
+        />
+      )}
+    </h1>
+  );
+}
+
+export default Chart;
